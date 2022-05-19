aae68b8d9e7a20013e5731c14445e5ef7d0e0e5a
Ginger Lee Kretschmer <gkretschmer@matterport.com>
2022-05-18 21:35:45 -0700

    Initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas"/>
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Sunt nulla velit qui ipsum tempor aliquip tempor aute nostrud. Do qui cillum minim excepteur deserunt in. Occaecat sit nulla officia voluptate id esse ad tempor. Magna nulla tempor mollit commodo magna laborum consectetur consequat eu ipsum cillum consectetur laborum. Sit labore ex officia tempor consequat ullamco. Consectetur cillum labore occaecat minim consequat fugiat. Aliqua nulla exercitation id nulla cillum non.</p>
+        <p>Sunt nulla velit qui ipsum tempor aliquip tempor aute nostrud. Do qui cillum minim excepteur deserunt in. Occaecat sit nulla officia voluptate id esse ad tempor. Magna nulla tempor mollit commodo magna laborum consectetur consequat eu ipsum cillum consectetur laborum. Sit labore ex officia tempor consequat ullamco. Consectetur cillum labore occaecat minim consequat fugiat. Aliqua nulla exercitation id nulla cillum non.</p>
+      </div>
+    </>
+    );
+}
+
+export default About;
